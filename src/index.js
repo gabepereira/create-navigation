972575f10668e606f0d55fc195cdbeeb2c3af623
gabepereira <gabriel6562014@gmail.com>
2019-11-12 19:02:51 -0300

    Created withRouter and exported it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
-import createNavigation from './createNavigation';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-export {
-    createNavigation
-}
+const withRouter = child => <BrowserRouter>{child}</BrowserRouter>;
+
+export default (views, config = Object) => {
+    Object.assign(config, {
+        root: config.root || '',
+        restrict: {
+            is: false,
+            redirect: '/',
+        },
+    });
+    const routes = [];
+    const { root, restrict } = config;
+    Object.keys(views).forEach(i => {
+        routes.push({
+            name: i,
+            component: views[i].component,
+            props: views[i].props,
+            path: views[i].path || `/${i.toLowerCase()}`,
+            exact: views[i].exact || false,
+            restrict:
+                views[i].restrict instanceof Object
+                    ? {
+                          is: views[i].restrict.is || restrict.is,
+                          redirect: views[i].restrict.redirect || null,
+                      }
+                    : views[i].restrict
+                    ? {
+                          ...restrict,
+                          is: true,
+                      }
+                    : restrict,
+        });
+    });
+
+    return (
+        <Switch>
+            {routes.map(({ component, path, restrict, exact }, i) => (
+                <Route
+                    key={i}
+                    exact={exact}
+                    path={root + path}
+                    render={props =>
+                        restrict.is ? (
+                            <Redirect
+                                to={{
+                                    pathname: restrict.redirect,
+                                    state: { from: props.location },
+                                }}
+                            />
+                        ) : (
+                            React.cloneElement(component, { ...props })
+                        )
+                    }
+                />
+            ))}
+        </Switch>
+    );
+};
+
+export { withRouter };
